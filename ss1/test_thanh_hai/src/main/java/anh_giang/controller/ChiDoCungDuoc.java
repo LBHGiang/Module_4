5ede3638fb1f331ecb952a13d21e6eb614c654b1
LBHGiang <hgno3001@gmail.com>
2022-10-12 16:46:46 +0700

    up test

---
+++ ss1/test_thanh_hai/src/main/java/anh_giang/controller/ChiDoCungDuoc.java
@@ -0,0 +1,12 @@
+package anh_giang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChiDoCungDuoc {
+    @GetMapping
+    public String ten(){
+        return "/anh_giang";
+    }
+}
